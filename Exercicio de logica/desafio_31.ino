78481bcd30346b122249ebbb5c6a4118f13e07ae
Walyson Menezes <menezeswalyson@gmail.com>
2025-05-15 10:22:30 -0300

    subir os desafios

---
+++ Exercicio de logica/desafio_31.ino
@@ -0,0 +1,12 @@
+void setup()
+{
+ Serial.begin(9600);
+}
+//função
+void loop()
+{
+  for(int contador = 0 ; contador <=100 ; contador += 2){
+    Serial.println ("Contador: " + String(contador));
+    delay(500);
+  }
+}
---
+++ Exercicio de logica/desafio_41.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for(int contador = 1 ; contador <=99 ; contador += 2){
+    Serial.println ("Contador: " + String(contador));
+    delay(500);
+  }
+  delay(2000);
+}
